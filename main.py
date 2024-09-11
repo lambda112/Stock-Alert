480a22c7b6a34b124fb26245ccf6a6a486c8e281
Kamran Azim <Kamazim121212@gmail.com>
2024-09-11 18:18:59 +0100

    Updated code

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ import os
 api_key = os.environ.get("api_key")
 url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSE&interval=5min&apikey={api_key}'
 r = requests.get(url)
+r.raise_for_status()
 
 data = r.json()["Time Series (Daily)"]
 last_two_days = dict(list(data.items())[:2])
-
+close_list = []
 
 for day in last_two_days.values():
     close_value = day["4. close"]
-    print(close_value)
+    close_list.append(close_value)
+
+print(close_list)
